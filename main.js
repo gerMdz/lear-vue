851cf52df27921c09b2dacb281679dd573184af4
gerardo montivero <gerardo.montivero@gmail.com>
2022-04-18 11:30:35 -0300

    Clase 20 v-if v-else v-else-if

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             titulo: 'Mi primer VueJs básico',
-            cantidad: 500,
+            cantidad: 1500,
             enlaceYoutube: 'https://www.youtube.com/bluuweb',
+            estado: false
         }
     }
 }).mount('#app');
