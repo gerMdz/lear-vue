44c2a08a1650ac68dadbabf8d5ff4dbbffa47063
gerardo montivero <gerardo.montivero@gmail.com>
2022-04-18 11:16:48 -0300

    Clase v-bind o reemplazo por ':'

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             titulo: 'Mi primer VueJs básico',
-            cantidad: 500
+            cantidad: 500,
+            enlaceYoutube: 'https://www.youtube.com/bluuweb',
         }
     }
 }).mount('#app');
