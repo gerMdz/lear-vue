ab9985dfcfc979d2ab4f293a19d4789762cd7ab3
gerardo montivero <gerardo.montivero@gmail.com>
2022-04-19 10:40:23 -0300

    Clase 23  desactivar

--- main.js
+++ main.js
@@ -5,12 +5,21 @@ const app = Vue.createApp({
             cantidad: 1500,
             enlaceYoutube: 'https://www.youtube.com/bluuweb',
             estado: false,
-            servicios: ['transferencias', 'pagos', 'giros', 'cheques']
+            servicios: ['transferencias', 'pagos', 'giros', 'cheques'],
+            desactivar: false
         }
     },
     methods: {
         agregarSaldo() {
             this.cantidad = this.cantidad + 100
+        },
+        disminuirSaldo(){
+            if(this.cantidad <= 0) {
+                alert('Saldo 0');
+                this.desactivar = true;
+                return;
+            }
+            this.cantidad = this.cantidad - 200
         }
     }
 }).mount('#app');
