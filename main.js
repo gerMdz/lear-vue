c7f0d38a708f0d737454132eb4a6c9467dff5836
gerardo montivero <gerardo.montivero@gmail.com>
2022-04-19 10:22:22 -0300

    Clase 21 v-for

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const app = Vue.createApp({
             titulo: 'Mi primer VueJs básico',
             cantidad: 1500,
             enlaceYoutube: 'https://www.youtube.com/bluuweb',
-            estado: false
+            estado: false,
+            servicios: ['transferencias', 'pagos', 'giros', 'cheques']
         }
     }
 }).mount('#app');
