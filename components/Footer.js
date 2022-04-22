9be0bf629ccaaaeeb988984d7c6d7f9dc58a69e6
gerardo montivero <gerardo.montivero@gmail.com>
2022-04-22 09:45:42 -0300

    Clase 30 comunicación entre componentes props

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,16 @@
 app.component('footer-banco', {
+    props: ['cantidad', 'fecha'],
     template: `
       <div class="bg-dark py-3 mt-2 text-white">
       <h3>
         {{ texto }} - {{ cantidad }}
       </h3>
+      <p>{{ fecha}}</p>
       </div>
     `,
     data() {
         return {
             texto: 'Pie de página del sitio dinámico un pco',
-            cantidad: 1000
         }
     }
 })
