6d85a68c00717d83a86846c6086ac2dc77e754e9
gerardo montivero <gerardo.montivero@gmail.com>
2022-04-22 09:25:21 -0300

    Clase 29 data en componentes

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,15 @@
 app.component('footer-banco', {
     template: `
-    <div class="bg-dark py-3 mt-2 text-white">
-        <h3>
-            Footer propio
-        </h3>
-    </div>
-    `
+      <div class="bg-dark py-3 mt-2 text-white">
+      <h3>
+        {{ texto }} - {{ cantidad }}
+      </h3>
+      </div>
+    `,
+    data() {
+        return {
+            texto: 'Pie de página del sitio dinámico un pco',
+            cantidad: 1000
+        }
+    }
 })
