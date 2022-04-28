8aafaba00a424eeb7b6a3458751210e5a9dccef7
gerardo montivero <gerardo.montivero@gmail.com>
2022-04-28 08:25:46 -0300

    Clase 36 Creando componente

--- hola-mundo-v2/src/App.vue
+++ hola-mundo-v2/src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App con BluuWeb"/>
+  <Titulo texto =' - dinámico 2.0'/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Titulo from "@/components/Titulo";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Titulo
   }
 }
 </script>
