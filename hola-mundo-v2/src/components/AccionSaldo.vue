85505d42d4f273fcc88bc97b01b010813fc56a47
gerardo montivero <gerardo.montivero@gmail.com>
2022-05-02 10:14:36 -0300

    Clase 38 Comunicación entre componentes padre <-> hijo

---
+++ hola-mundo-v2/src/components/AccionSaldo.vue
@@ -0,0 +1,14 @@
+<template>
+<button>Aumentar / Disuminuir Saldo</button>
+
+</template>
+
+<script>
+export default {
+  name: "AccionSaldo"
+}
+</script>
+
+<style scoped>
+
+</style>
--- hola-mundo-v2/src/components/Cuenta.vue
+++ hola-mundo-v2/src/components/Cuenta.vue
@@ -1,16 +1,22 @@
 <template>
   <h2>Tipo: {{ cuenta }}</h2>
   <h2>Saldo: {{ saldo }}</h2>
-  <h2>Cuenta: {{ estado ? 'Activa': 'Desactivada' }}</h2>
+  <h2>Cuenta: {{ estado ? 'Activa' : 'Desactivada' }}</h2>
   <div v-for="(servicio, index) in servicios" :key="index">
-    {{ index + 1  }} {{ servicio}}
+    {{ index + 1 }} {{ servicio }}
   </div>
-
+  <AccionSaldo/>
+  <AccionSaldo/>
 </template>
 
 <script>
+import AccionSaldo from "@/components/AccionSaldo";
+
 export default {
   name: "Cuenta",
+  components: {
+    AccionSaldo
+  },
   data() {
     return {
       cuenta: 'Visa',
@@ -18,12 +24,22 @@ export default {
       servicios: ['transferencias', 'pagos', 'giros', 'cheques'],
       saldo: 1000,
     }
+  },
+  methods: {
+    aumentar(){
+      this.saldo = this.saldo + 100
+    },
+    disminuir() {
+      this.saldo = this.saldo - 70
+    }
   }
+
+
 }
 </script>
 
 <style scoped>
-h1, h2{
+h1, h2 {
   color: #0f61ad;
 }
 
