d300d2d1ea4de7c8341938bd859757807128c9e9
gerardo montivero <gerardo.montivero@gmail.com>
2022-05-10 09:32:19 -0300

    Clase 48 First View

--- router-udemy_b-1/src/App.vue
+++ router-udemy_b-1/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/blog">Blog</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ router-udemy_b-1/src/views/Blog.vue
@@ -0,0 +1,14 @@
+<template>
+  blog
+
+</template>
+
+<script>
+export default {
+  name: "Blog"
+}
+</script>
+
+<style scoped>
+
+</style>
