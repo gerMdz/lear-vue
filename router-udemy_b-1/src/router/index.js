08865179624fef957808e2c3c47cfcdfe1aeb740
gerardo montivero <gerardo.montivero@gmail.com>
2022-05-11 09:58:02 -0300

    Cap 50 Rutas con parámetros

--- router-udemy_b-1/src/router/index.js
+++ router-udemy_b-1/src/router/index.js
@@ -22,6 +22,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Blog.vue')
+  },
+  {
+    path: '/blog/:id',
+    name: 'Articulo',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Articulo.vue')
   }
 ]
 
---
+++ router-udemy_b-1/src/views/Articulo.vue
@@ -0,0 +1,18 @@
+<template>
+  <Titulo texto="Ruta con parámetros" />
+  <h2>Parametro {{ $route.params.id }}</h2>
+</template>
+
+<script>
+import Titulo from "@/components/Titulo";
+export default {
+  name: "Articulo",
+  components: {
+    Titulo
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
