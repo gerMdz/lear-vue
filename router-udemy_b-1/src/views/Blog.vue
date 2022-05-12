1a3991ce4bb75305a3b94693399ead441460e97c
gerardo montivero <gerardo.montivero@gmail.com>
2022-05-12 10:54:25 -0300

    Cap 52 Consumir api y mostrar en pantall

--- router-udemy_b-1/src/views/Blog.vue
+++ router-udemy_b-1/src/views/Blog.vue
@@ -1,14 +1,36 @@
 <template>
   <Titulo texto="Título de mi blog" />
+  <button @click="consumerApi"> Consumer API </button>
+  <ul v-for="item in arrayBlog" :key="item.id">
+    <li>{{ item.title }}</li>
+  </ul>
+
 
 </template>
 
 <script>
 import Titulo from "@/components/Titulo";
+
 export default {
   name: "Blog",
   components: {
     Titulo
+  },
+  data() {
+    return {
+      arrayBlog: []
+    }
+  }
+  ,
+  methods: {
+    async consumerApi() {
+      try {
+        const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+        this.arrayBlog = await data.json()
+      } catch (e) {
+        console.log(e)
+      }
+    }
   }
 }
 </script>
