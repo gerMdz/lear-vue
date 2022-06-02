e744f00284b69467ff16c7c651e950c20cfd9dce
gerardo montivero <gerardo.montivero@gmail.com>
2022-06-02 07:53:51 -0300

    State first steep

--- vuex-1/src/views/AboutView.vue
+++ vuex-1/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Contador {{ $store.state.contador}}</h2>
   </div>
 </template>
--- vuex-1/src/views/HomeView.vue
+++ vuex-1/src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Contador {{ $store.state.contador}}</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
