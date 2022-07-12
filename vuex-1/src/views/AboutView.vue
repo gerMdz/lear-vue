cb8924597716d29bf1a64fe199bda1a0c396fb8d
gerMDZ Gerardo Montivero <gerardo.montivero@gmail.com>
2022-07-12 09:05:08 -0300

    Una mirada simple a v-model

--- vuex-1/src/views/AboutView.vue
+++ vuex-1/src/views/AboutView.vue
@@ -3,6 +3,9 @@
     <h1>This is an about page</h1>
     <h2>Contador {{ $store.state.contador}}</h2>
     <BtnDisminuir />
+    <input type="text" v-model="texto">
+    <h2> {{texto}} </h2>
+
   </div>
 </template>
 <script>
@@ -12,5 +15,10 @@ export default {
   components: {
     BtnDisminuir
   },
+  data(){
+    return {
+      texto: 'Mi texto'
+    }
+  }
 }
 </script>
