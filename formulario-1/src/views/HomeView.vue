4bc2bebba2f3d5e432c0a5638f8b38370bed881a
gerMDZ Gerardo Montivero <gerardo.montivero@gmail.com>
2022-07-13 07:42:06 -0300

    Iniciando formulario-1

---
+++ formulario-1/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    Inicio
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
