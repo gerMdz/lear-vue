39ea84543670b61d997a2f382fb838183c9892d8
gerMDZ Gerardo Montivero <gerardo.montivero@gmail.com>
2022-07-14 08:14:32 -0300

    Checkbox and vmodel

--- formulario-1/src/views/HomeView.vue
+++ formulario-1/src/views/HomeView.vue
@@ -6,6 +6,28 @@
         placeholder="Ingrese nombre"
         v-model="tarea.nombre"
     >
+    <div class="form-check form-check-inline">
+      <input
+      type="checkbox"
+      id="check-1"
+      class="form-check-input"
+      v-model="tarea.categorias"
+      value="JScript"
+      >
+      <label for="check-1" class="form-check-label"> JScript</label>
+
+    </div>
+    <div class="form-check form-check-inline">
+      <input
+      type="checkbox"
+      id="check-2"
+      class="form-check-input"
+      v-model="tarea.categorias"
+      value="Node"
+      >
+      <label for="check-2" class="form-check-label"> Node</label>
+
+    </div>
   </form>
   <p>{{tarea}}</p>
 </template>
@@ -22,7 +44,8 @@ export default {
   data(){
     return {
       tarea: {
-        nombre: ''
+        nombre: '',
+        categorias: []
       }
     }
   }
