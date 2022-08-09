764134feff47a57512e9b404df628db157eaff8b
gerMDZ Gerardo Montivero <gerardo.montivero@gmail.com>
2022-08-09 09:21:28 -0300

    Tareas en base de datos

--- formulario-1/src/store/index.js
+++ formulario-1/src/store/index.js
@@ -20,11 +20,9 @@ export default createStore({
     },
     set(state, payload){
       state.tareas.push(payload)
-      localStorage.setItem('tareas', JSON.stringify(state.tareas))
     },
     eliminar(state, payload){
       state.tareas = state.tareas.filter(item => item.id !== payload)
-      localStorage.setItem('tareas', JSON.stringify(state.tareas))
     },
     tarea(state, payload){
       if(!state.tareas.find(item => item.id === payload)){
@@ -36,19 +34,26 @@ export default createStore({
     update(state, payload){
       state.tareas = state.tareas.map(item => item.id === payload.id? payload: item)
       router.push('/').then(r => {})
-      localStorage.setItem('tareas', JSON.stringify(state.tareas))
     }
   },
   actions: {
     cargarLocalStorage({commit}){
-        if(localStorage.getItem('tareas')){
-          const tareas = JSON.parse(localStorage.getItem('tareas'))
-          commit('cargar', tareas)
-          return
-        }
-        localStorage.setItem('tareas', JSON.stringify([]))
     },
-    setTareas({commit}, tarea){
+    async setTareas({commit}, tarea){
+      try {
+        const res = await fetch(`https://api-vue-udemy-3b734-default-rtdb.firebaseio.com/tareas/${tarea.id}.json`,{
+          method:'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(tarea),
+        })
+
+        const dataDB = await res.json()
+        console.log(dataDB)
+      }catch (e){
+        console.log(e)
+      }
       commit('set', tarea)
     },
     deleteTareas({commit}, id){
