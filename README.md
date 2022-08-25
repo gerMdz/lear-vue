16eb351f1be1271c398c81c36ea2a6ea966b6dfc
gerardo montivero <gerardo.montivero@gmail.com>
2022-08-25 09:01:26 -0300

    Var let const iniciando

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # lear-vue
 
 [Vue-udemy](https://bluuweb.github.io/vue-udemy/)
+
+[JS - Fundamentos](https://bluuweb.github.io/javascript/fundamentos/)
---
+++ formulario-1/src/libs/index.js
@@ -0,0 +1,18 @@
+// var permite re-declarar variables y reasignar valores
+// var edad = 10;
+// var edad = 20;
+
+// let no permite re-declarar variables pero si reasignar valores
+// let edad = 10;
+// edad = 20;
+
+// const no permite re-declarar variables ni reasignar valores
+const edad = 10;
+
+// Ojo con el scope de los tipos de variable
+
+
+
+
+
+console.log(edad);
