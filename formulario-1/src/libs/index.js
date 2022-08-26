97b6be76758aa159625bc7c92b5ccd2de8ab3e06
gerardo montivero <gerardo.montivero@gmail.com>
2022-08-26 16:50:29 -0300

    Funciones de flecha y tradicionales

--- formulario-1/src/libs/index.js
+++ formulario-1/src/libs/index.js
@@ -8,11 +8,61 @@
 
 // const no permite re-declarar variables ni reasignar valores
 const edad = 10;
+console.log(edad);
 
 // Ojo con el scope de los tipos de variable
 
+const persona =  {
+  nombre:'julio'
+}
+
+console.log(persona)
+
+persona.nombre = 'Gerardo'
+
+console.log(persona)
+
+function sumar (num){
+    console.log(num)
+}
+
+sumar(25)
 
 
+const sumarDos = (num1,num2 ) => {
+  console.log(num1 + num2)
+}
+
+sumarDos(30, 40)
+
+const sumarTres = num => {
+    console.log(num)
+}
+
+sumarTres(35)
+
+const sumarCuatro = (num1,num2 ) => {
+    return (num1 + num2)
+}
+const resultado = sumarCuatro(75, 80)
+console.log(resultado)
 
 
-console.log(edad);
+const sumarCinco = (num1,num2 ) => (num1 + num2)
+const resultado2 = sumarCinco(160, 165)
+console.log(resultado2)
+
+const mensaje = (nombre) => {
+  return 'I\'m ' + nombre
+}
+
+
+const resultado3 = mensaje('Gerardo')
+
+console.log(resultado3)
+
+const suma3 = (num= 0) => {
+  console.log(num + 3)
+}
+
+suma3();
