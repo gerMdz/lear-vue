3bdadfd013b0e2ceca53239321af34a0301b665d
gerardo montivero <gerardo.montivero@gmail.com>
2022-09-22 08:56:35 -0300

    :bug: Recorriendo objetos con v-for

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,33 +1,101 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
 const name = 'VUE dinámico'
 const styleColor = "color: #1a1c80"
 const arrayColores = ["blue", "red", "peru"]
 const active = false
+
+const arrayProductos = [
+  {
+    name: "Manzana",
+    price: "$1.00",
+    description: "por kilo",
+  },
+  {
+    name: "Huevos",
+    price: "$2.00",
+    description: "por Docena",
+  },
+  {
+    name: "Melón",
+    price: "$3.00",
+    description: "por Unidad",
+  }
+]
+
+const objetoProducto =
+    {
+      name: "mango",
+      price: "$4.00",
+      description: "por Unidad",
+      id: 1,
+    }
+
+const arrayFrutas = ["murder", "surprise", "forest", "month", "decrease"];
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125"/>
 
     <div class="wrapper">
-      <HelloWorld msg="Iniciando" />
+      <HelloWorld msg="Iniciando"/>
     </div>
   </header>
   <section>
-    <h1 > Hola desde Mendoza <br/> {{name.toUpperCase()}}</h1>
-    <h2>{{arrayColores}}</h2>
+    <h1> Hola desde Mendoza <br/> {{ name.toUpperCase() }}</h1>
+    {{ objetoProducto }}
+
+    <ul>
+      <li
+          v-for="(value , propiedades, index) in objetoProducto"
+          :key="value"
+      >
+        [{{ index }}] {{ propiedades }}= {{ value }}
+      </li>
+    </ul>
+    <h2>{{ arrayColores }}</h2>
     <h2 :style="`color: ${arrayColores[2]}`"> Azul </h2>
-    <h3> {{active ? 'si' : 'no'}} </h3>
-    <p > <span v-if="active"> Si </span><span v-else> No </span> Estoy activo</p>
+    <h3> {{ active ? 'si' : 'no' }} </h3>
+    <p><span v-if="active"> Si </span><span v-else> No </span> Estoy activo</p>
 
     <p v-show="active"> Por show</p>
+
+
+    <!--    <ul>-->
+    <!--      <li-->
+    <!--          v-for="(fruta, index) in arrayFrutas"-->
+    <!--          :key="index"-->
+    <!--      >-->
+    <!--       {{fruta}}-->
+    <!--      </li>-->
+    <!--    </ul>-->
+
+    <ul>
+      <li v-for="producto in arrayProductos"
+          :key="producto.name"
+      >
+        <!--        {{producto.name}}-->
+        <ul>
+          <li v-for="(value , propiedades) in producto"
+              :key="value">
+            <ul>
+              <li> {{ propiedades }}: {{ value }}</li>
+
+            </ul>
+          </li>
+        </ul>
+      </li>
+
+
+    </ul>
 
   </section>
 
   <main>
-    <TheWelcome />
+    <TheWelcome/>
   </main>
 </template>
 
@@ -40,9 +108,11 @@ header {
   display: block;
   margin: 0 auto 2rem;
 }
+
 h1 {
   color: red;
 }
+
 @media (min-width: 1024px) {
   header {
     display: flex;
