bcb2eee21fa4c7a8a3eb7fb29e1fc0f0c7e9d2fb
Gerardo Montivero <gmontivero@jus.mendoza.gov.ar>
2023-05-09 08:56:17 -0300

    agregando

---
+++ vuex-1/components-1/src/components/PaginatePost.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+<template>
+  <div class="btn-group" role="group" aria-label="Basic example">
+    <button type="button" class="btn btn-outline-primary">Anterior</button>
+    <button type="button" class="btn btn-outline-primary">Siguiente</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
